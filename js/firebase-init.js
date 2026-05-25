585f2e47ccd82d84d1b6c463cc2c6589fe2dedfe
weiiiik12 <haowork12@gmail.com>
2026-05-25 15:20:06 +0800

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -4,13 +4,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 已將專案 ID 修改為 hago-award-system
 const firebaseConfig = {
-    apiKey: "AIzaSyCArCzhRG6H-07Ooet34ikyP3w9xVq6t1U",
-    authDomain: "kidrewardapp.firebaseapp.com",
-    projectId: "kidrewardapp",
-    storageBucket: "kidrewardapp.firebasestorage.app",
-    messagingSenderId: "980449979485",
-    appId: "1:980449979485:web:652d6b419385e76ad071b4",
+    apiKey: "AIzaSyCArCzhRG6H-07Ooet34ikyP3w9xVq6t1U", // 註：若此金鑰無法連線，請至新專案後台複製新的 apiKey 替換
+    authDomain: "hago-award-system.firebaseapp.com",
+    projectId: "hago-award-system",
+    storageBucket: "hago-award-system.appspot.com", // Firebase 預設通常為 .appspot.com 或 .firebasestorage.app
+    messagingSenderId: "980449979485", // 建議同步替換為新專案的發送者 ID
+    appId: "1:980449979485:web:652d6b419385e76ad071b4", // 建議同步替換為新專案的應用程式 ID
     measurementId: "G-L86Y9Y477Y"
 };
 
