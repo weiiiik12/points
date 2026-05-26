6334ab6c30bb4c7dc6cf3ac780701739c2f75466
weiiiik12 <haowork12@gmail.com>
2026-05-26 14:01:50 +0800

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,26 @@
+// js/questions.js
+
+// 這裡可以自由增加題目，支援單選題
+export const DAILY_QUESTIONS = [
+    {
+        id: 1,
+        question: "下列哪一個單字是『學校』的意思？",
+        options: ["Apple", "School", "Teacher", "Book"],
+        answer: 1, // 代表 options[1] 的 "School" 是正確答案
+        points: 20 // 答對獲得 20 點
+    },
+    {
+        id: 2,
+        question: "老師在英文課常說的『Listen carefully』是什麼意思？",
+        options: ["大聲朗讀", "仔細聆聽", "請回座位", "打開課本"],
+        answer: 1,
+        points: 20
+    },
+    {
+        id: 3,
+        question: "英文句子開頭的第一個字母通常要如何處理？",
+        options: ["維持小寫", "全部加底線", "一定要大寫", "隨便都可以"],
+        answer: 2,
+        points: 20
+    }
+];
